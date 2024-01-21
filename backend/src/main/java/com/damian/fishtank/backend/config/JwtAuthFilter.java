e09a8edc9fdd280420cb5482930e6aff05df1d58
Damian Dawidowski <damiandawidowski@gmail.com>
2024-01-21 18:37:27 +0100

    modified:   backend/src/main/java/com/damian/fishtank/backend/config/JwtAuthFilter.java

--- backend/src/main/java/com/damian/fishtank/backend/config/JwtAuthFilter.java
+++ backend/src/main/java/com/damian/fishtank/backend/config/JwtAuthFilter.java
@@ -19,31 +19,28 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain) throws ServletException, IOException {
-        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
-
-        if (header != null) {
-            String[] authElements = header.split(" ");
-
-            if (authElements.length == 2
-                    && "Bearer".equals(authElements[0])) {
-                try {
-                    if ("GET".equals(request.getMethod())) {
-                        SecurityContextHolder.getContext().setAuthentication(
-                                userAuthenticationProvider.validateToken(authElements[1]));
-                    } else {
-                        SecurityContextHolder.getContext().setAuthentication(
-                                userAuthenticationProvider.validateTokenStrongly(authElements[1]));
-                    }
-                } catch (RuntimeException e) {
-                    SecurityContextHolder.clearContext();
-                    throw e;
+        HttpServletRequest request,
+        HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+            String header = request.getHeader(HttpHeaders.AUTHORIZATION); 
+            if (header != null) {
+                String[] authElements = header.split(" "); 
+                if (authElements.length == 2
+                        && "Bearer".equals(authElements[0])) {
+                            try {
+                                if ("GET".equals(request.getMethod())) {
+                                    SecurityContextHolder.getContext().setAuthentication(
+                                            userAuthenticationProvider.validateToken(authElements[1]));
+                                } else {
+                                    SecurityContextHolder.getContext().setAuthentication(
+                                            userAuthenticationProvider.validateTokenStrongly(authElements[1]));
+                                }
+                            } catch (RuntimeException e) {
+                                SecurityContextHolder.clearContext();
+                                throw e;
+                            }
                 }
-            }
-        }
-
+            } 
         filterChain.doFilter(request, response);
     }
 }
